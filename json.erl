0da5e7c4a0d226aa2fe1d545097d95379a554f49
Yana Timoshenko <yana@riseup.net>
2015-04-13 15:45:31 +0700

    Refactor
    Change internal API, handle floats in encode/1, handle special
    characters more carefully.

--- json.erl
+++ json.erl
@@ -9,6 +9,7 @@
 %% When decoding, everything is vice versa except strings. Strings are
 %% parsed to lists. You can alter this behavior to fit your preferences
 %% by editing decode_string/2 in this file.
+
 -module(json).
 
 -export([encode/1, decode/1]).
@@ -16,16 +17,24 @@
 -define(is_space(X), X == $\t; X == $\s; X == $\t; X == $\n).
 
 encode(Bin) when is_binary(Bin) ->
-    EscapeCodes = [<<"\b">>, <<"\f">>, <<"\n">>, <<"\r">>, <<"\t">>, <<"\"">>, <<"\\">>],
-    EscapedBin = binary:replace(Bin, EscapeCodes, <<"\\">>, [{insert_replaced,1}]), 
+    EscapedBin = escape(Bin), 
     <<"\"", EscapedBin/binary, "\"">>; 
 encode(I) when is_integer(I) -> integer_to_binary(I);
+encode(F) when is_float(F) -> float_to_binary(F);
 encode(M) when is_map(M) -> encode_map(maps:to_list(M), []);
 encode(L) when is_list(L) -> encode_list(L, []);
 encode(true) -> <<"true">>;
 encode(false) -> <<"false">>;
 encode(null) -> <<"null">>;
 encode(A) when is_atom(A) -> encode(list_to_binary(atom_to_list(A))).
+
+escape(Bin) ->
+    Substitutions = [
+        {<<"\\">>, <<"\\\\">>}, {<<"\"">>, <<"\\\"">>},
+        {<<"\f">>, <<"\\f">>}, {<<"\n">>, <<"\\n">>},  
+        {<<"\r">>, <<"\\r">>}, {<<"\t">>, <<"\\t">>}
+    ],
+    lists:foldl(fun({K,V},Acc) -> binary:replace(Acc, K, V) end, Bin, Substitutions).
 
 %% Wraps a list of binaries with First and Last elements and concatenates them all.
 wrap(List, First, Last) -> iolist_to_binary([First | lists:reverse([Last | List])]).
@@ -44,42 +53,39 @@ encode_pair({K,V}) ->
     Key = encode(K), Value = encode(V), 
     <<Key/binary, ":", Value/binary>>.
 
-decode(String) -> {Value, _} = decode(String, []), Value.
-
-decode(Bin, Buf) when is_binary(Bin) -> decode(binary_to_list(Bin), Buf);
+decode(Bin) when is_binary(Bin) -> decode(binary_to_list(Bin));
+decode(String) -> {_, Value} = decode(String, []), Value.
 
 decode([$"|T], []) -> decode_string(T, []);
-decode([$[|T], []) ->
-    [Rest|List] = lists:reverse(decode_list(T)),
-    {lists:reverse(List), Rest};
-
+decode([$[|T], []) -> decode_list(T, []);
 decode([${|T], []) -> decode_map(T, #{});
+
 decode([H|T], []) when ?is_space(H) -> decode(T, []);
 
 decode([$t|T], []) -> decode(T, "t");
 decode([$r|T], "t") -> decode(T, "tr");
 decode([$u|T], "tr") -> decode(T, "tru");
-decode([$e|T], "tru") -> {true, T};
+decode([$e|T], "tru") -> {T, true};
 
 decode([$f|T], []) -> decode(T, "f");
 decode([$a|T], "f") -> decode(T, "fa");
 decode([$l|T], "fa") -> decode(T, "fal");
 decode([$s|T], "fal") -> decode(T, "fals");
-decode([$e|T], "fals") -> {false, T};
+decode([$e|T], "fals") -> {T, false};
 
 decode([$n|T], []) -> decode(T, "n");
 decode([$u|T], "n") -> decode(T, "nu");
 decode([$l|T], "nu") -> decode(T, "nul");
-decode([$l|T], "nul") -> {null, T};
+decode([$l|T], "nul") -> {T, null};
 
 decode(Chars=[H|_], []) when ?is_digit(H) -> decode_integer(Chars, []).
 
 decode_integer([H|T], Buf) when ?is_digit(H) -> decode_integer(T, [H|Buf]);
 decode_integer([$.|T], Buf) -> decode_float(T, [$.|Buf]);
-decode_integer(Chars, Buf) -> {list_to_integer(lists:reverse(Buf)), Chars}.
+decode_integer(Chars, Buf) -> {Chars, list_to_integer(lists:reverse(Buf))}.
 
 decode_float([H|T], Buf) when ?is_digit(H) -> decode_float(T, [H|Buf]);
-decode_float(Chars, Buf) -> {list_to_float(lists:reverse(Buf)), Chars}.
+decode_float(Chars, Buf) -> {Chars, list_to_float(lists:reverse(Buf))}.
 
 decode_string([$\\|Chars], Buf) ->
     Char = hd(Chars),
@@ -92,24 +98,24 @@ decode_string([$\\|Chars], Buf) ->
         Char -> Char
     end,
     decode_string(tl(Chars), [EscapeCode|Buf]);
-decode_string([$"|Chars], Buf) -> {lists:reverse(Buf), Chars};
+decode_string([$"|Chars], Buf) -> {Chars, lists:reverse(Buf)};
 decode_string([H|T], Buf) -> decode_string(T, [H|Buf]).
 
-decode_list([$] |Chars]) -> [Chars];
-decode_list([$, |Chars]) -> decode_list(Chars);
-decode_list([H|T]) when ?is_space(H) -> decode_list(T);
-decode_list(Chars) ->
-    {Value, Rest} = decode(Chars, []),
-    [Value|decode_list(Rest)].
+decode_list([$] |Chars], List) -> {Chars, lists:reverse(List)};
+decode_list([$, |Chars], List) -> decode_list(Chars, List);
+decode_list([H|T], List) when ?is_space(H) -> decode_list(T, List);
+decode_list(Chars, List) ->
+    {Rest, Value} = decode(Chars, []),
+    decode_list(Rest, [Value|List]).
 
-decode_map([$}|Chars], Map) -> {Map, Chars};
+decode_map([$}|Chars], Map) -> {Chars, Map};
 decode_map([$,|Chars], Map) -> decode_map(Chars, Map);
 decode_map([H|T], Map) when ?is_space(H) -> decode_map(T, Map);
 decode_map(Chars, Map) ->
-    {Key, Rest} = decode(Chars, []),
-    {ok, Rest2} = decode_colon(Rest),
-    {Value, Rest3} = decode(Rest2, []),
+    {Rest1, Key} = decode(Chars, []),
+    {Rest2, ok} = decode_colon(Rest1),
+    {Rest3, Value} = decode(Rest2, []),
     decode_map(Rest3, maps:put(Key, Value, Map)).
 
-decode_colon([$:|T]) -> {ok, T};
+decode_colon([$:|T]) -> {T, ok};
 decode_colon([H|T]) when ?is_space(H) -> decode_colon(T).

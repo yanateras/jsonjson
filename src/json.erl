3ea34d966fe62dcf5eecfd528e9684e4bfa6e8fe
Yana Timoshenko <yana@riseup.net>
2015-08-23 16:54:57 +0000

    Overhaul the decoder + encoder float fix
    Includes the following improvements:
    
    	1. Adds error handling. From now on, when decoder fails,
    	   it will return one of the following reasons:
    	   * invalid_escape if a UTF-8 escape sequence is invalid
    	     or a single unpaired UTF-16 surrogate is encountered;
    	     {error, invalid_escape} = json:decode(<<"\\uDEAD">>).
    
    	   * missing_colon if a colon doesn't follow a key;
    	     {error, missing_colon} = json:decode(<<"{\"x\" 1}>>).
    
    	   * unexpected_token when a JSON value syntax is invalid;
    	     {error, unexpected_token} = json:decode(<<"[xyz]>>").
    
    	   * unterminated_array if an array is not terminated;
    	     {error, unterminated_array} = json:decode(<<"[1">>).
    
    	   * unterminated_exponent if an exponent is not terminated;
    	     {error, unterminated_exponent} = json:decode(<<"1e">>).
    
    	   * unterminated_fraction if a fraction is not terminated;
    	     {error, unterminated_fraction} = json:decode(<<"1.">>).
    
    	   * unterminated_integer if an integer has only a sign;
    	     {error, unterminated_integer} = json:decode(<<"-">>).
    
    	   * unterminated_object if an object is not terminated;
    	     {error, unterminated_object} = json:decode(<<"{">>).
    
    	   * unterminated_string if a string is not terminated;
    	     {error, unterminated_string} = json:decode(<<"\"hi">>).
    
    	   All other error reasons are invalid. If you encounter one,
    	   please open an issue.
    
    	2. Stricter number decoding. Previously, the following match
    	   would fail, and now it doesn't:
    	   {ok, 1, <<"-1">>} = json:decode(<<"1-1">>).
    
    	3. Now decoder allows numbers to be prefixed with + sign:
    	   {ok, 1, _} = json:decode(<<"+1">>).
    
    	4. Correct function names. Previously decoder functions were
    	   named after Erlang terms, while they really should be
    	   named after JSON entities. Affected functions:
    	   * decode_list/2 -> decode_array/2
    	   * decode_map/2 -> decode_object/2
    	   * decode_float/2 -> decode_fraction/2
    	   * decode_exponent/2 extracted
    	   * decode_number/2 extracted
    	   * decode_sign/2 extracted
    
    	5. Commas now are globally considered whitespace:
    	   {ok, #{<<"x">>:=0}, _} = json:decode(<<",{,\"x\",:,0}">>).
    
    	6. Instead of unicode:characters_to_binary/1, decode/1 now
    	   uses list_to_binary/1 to convert binary into string.
    
    	7. When passing a wrong value into decode/1, instead of
    	   returning {error, badarg} it fails with badarg.
    
    	8. Return value of decode_colon/2 has changed from ok to $:.
    
    	9. Encoder now encodes floats correctly.
    
    	10. Minor improvements in manual page.

--- src/json.erl
+++ src/json.erl
@@ -14,12 +14,13 @@
 
 -export([encode/1, decode/1]).
 -define(is_digit(X), X >= 48, X =< 57).
--define(is_space(X), X =< 32).
--define(is_exponent(X), X == $e; X == $E; X == $+; X == $-).
+-define(is_space(X), X =< 32; X == $,).
+-define(is_exponent(X), X == $e; X == $E).
+-define(is_sign(X), X == $+; X == $-).
 
 encode(Bin) when is_binary(Bin) -> encode_string(Bin, <<$">>);
 encode(I) when is_integer(I) -> integer_to_binary(I);
-encode(F) when is_float(F) -> float_to_binary(F);
+encode(F) when is_float(F) -> io_lib:format("~p", [F]);
 encode(M) when is_map(M) -> encode_map(maps:to_list(M), []);
 encode([{_, _}, _] = Props) -> encode_map(Props, []);
 encode(L) when is_list(L) -> encode_list(L, []);
@@ -47,46 +48,71 @@ encode_map([H|T], Buf) -> encode_map(T, [$, | [encode_pair(H) | Buf]]).
 
 encode_pair({K,V}) -> [encode(K), $:, encode(V)].
 
-decode(String) when is_list(String) -> decode(unicode:characters_to_binary(String));
+decode(String) when is_list(String) -> decode(list_to_binary(String));
 decode(Bin) when is_binary(Bin) ->
-    try
-        decode_value(Bin)
-    of
+    try decode_value(Bin) of
         {Rest, Value} -> {ok, Value, Rest}
-    catch
-       error:Reason -> {error, Reason} 
-    end.
+    catch error:Reason -> {error, Reason} 
+    end;
+decode(_) -> error(badarg).
 
 decode_value(<<$", T/binary>>) -> decode_string(T, <<>>);
-decode_value(<<$[, T/binary>>) -> decode_list(T, []);
-decode_value(<<${, T/binary>>) -> decode_map(T, #{});
+decode_value(<<$[, T/binary>>) -> decode_array(T, []);
+decode_value(<<${, T/binary>>) -> decode_object(T, #{});
 
-decode_value(<<H, T/binary>>) when ?is_digit(H); H == $- -> decode_integer(T, [H]);
+decode_value(<<H, T/binary>>) when ?is_digit(H); ?is_sign(H) -> decode_number(T, [H]);
 decode_value(<<H, T/binary>>) when ?is_space(H) -> decode_value(T);
 
 decode_value(<<"true", T/binary>>) -> {T, true};
 decode_value(<<"false", T/binary>>) -> {T, false};
-decode_value(<<"null", T/binary>>) -> {T, null}.
+decode_value(<<"null", T/binary>>) -> {T, null};
+
+decode_value(_) -> error(unexpected_token).
+
+decode_number(Bin, Buf) ->
+    {Rest, Acc} = decode_sign(Bin, Buf), 
+    decode_integer(Rest, Acc).
 
 decode_integer(<<H, T/binary>>, Buf) when ?is_digit(H) -> decode_integer(T, [H|Buf]);
-decode_integer(<<$., T/binary>>, Buf) -> decode_float(T, [$.|Buf]);
+decode_integer(<<$., T/binary>>, Buf) -> decode_fraction(T, [$.|Buf]);
+decode_integer(<<H, T/binary>>, Buf) when ?is_exponent(H) ->
+    {Rest1, Acc} = decode_sign(T, [H, $0, $.] ++ Buf),
+    decode_exponent(Rest1, Acc);
+decode_integer(_, [H|_]) when ?is_sign(H) -> error(unterminated_integer);
 decode_integer(Bin, Buf) -> {Bin, list_to_integer(lists:reverse(Buf))}.
 
-decode_float(<<H, T/binary>>, Buf) when ?is_digit(H); ?is_exponent(H) -> decode_float(T, [H|Buf]);
-decode_float(Bin, Buf) -> {Bin, list_to_float(lists:reverse(Buf))}.
+decode_fraction(<<H, T/binary>>, Buf) when ?is_digit(H) -> decode_fraction(T, [H|Buf]);
+decode_fraction(<<H, T/binary>>, Buf) when ?is_exponent(H) ->
+    {Rest, Acc} = decode_sign(T, [H|Buf]),
+    decode_exponent(Rest, Acc);
+decode_fraction(_, [H|_]) when H == $.; ?is_sign(H) -> error(unterminated_fraction);
+decode_fraction(Bin, Buf) -> {Bin, list_to_float(lists:reverse(Buf))}.
+
+decode_exponent(<<H, T/binary>>, Buf) when ?is_digit(H) -> decode_exponent(T, [H|Buf]);
+decode_exponent(_, [H|_]) when ?is_exponent(H); ?is_sign(H) -> error(unterminated_exponent);
+decode_exponent(Bin, Buf) ->
+    try list_to_float(lists:reverse(Buf)) of
+        Float -> {Bin, Float}
+    catch error:badarg -> error(infinity)
+    end.
+
+decode_sign(<<H, T/binary>>, Buf) when ?is_sign(H) -> {T, [H|Buf]};
+decode_sign(Bin, Buf) -> {Bin, Buf}.
 
 decode_string(<<$", T/binary>>, Buf) -> {T, Buf};
 decode_string(<<$\\, $u, C1, C2, C3, C4, T/binary>>, Buf) ->
-    C = list_to_integer([C1, C2, C3, C4], 16),
-    if C > 16#D7FF, C < 16#DC00 ->
-        <<$\\, $u, D1, D2, D3, D4, T2/binary>> = T,
-        D = list_to_integer([D1, D2, D3, D4], 16),
-        Point = xmerl_ucs:from_utf16be(<<C:16/big-unsigned-integer, D:16/big-unsigned-integer>>),
-        Char = unicode:characters_to_binary(Point),
-        decode_string(T2, <<Buf/binary, Char/binary>>);
-    true ->
-        Char = unicode:characters_to_binary([C]),
-        decode_string(T, <<Buf/binary, Char/binary>>)
+    try C = list_to_integer([C1, C2, C3, C4], 16),
+        if C > 16#D7FF, C < 16#DC00 ->
+            <<$\\, $u, D1, D2, D3, D4, T2/binary>> = T,
+            D = list_to_integer([D1, D2, D3, D4], 16),
+            Point = xmerl_ucs:from_utf16be(<<C:16/big-unsigned-integer, D:16/big-unsigned-integer>>),
+            Char = unicode:characters_to_binary(Point),
+            decode_string(T2, <<Buf/binary, Char/binary>>);
+        true ->
+            Char = unicode:characters_to_binary([C]),
+            decode_string(T, <<Buf/binary, Char/binary>>)
+        end
+    catch error:badarg -> error(invalid_escape)
     end;
 decode_string(<<$\\, $b, T/binary>>, Buf) -> decode_string(T, <<Buf/binary, $\s>>);
 decode_string(<<$\\, $f, T/binary>>, Buf) -> decode_string(T, <<Buf/binary, $\f>>);
@@ -94,21 +120,25 @@ decode_string(<<$\\, $n, T/binary>>, Buf) -> decode_string(T, <<Buf/binary, $\n>
 decode_string(<<$\\, $r, T/binary>>, Buf) -> decode_string(T, <<Buf/binary, $\r>>);
 decode_string(<<$\\, $t, T/binary>>, Buf) -> decode_string(T, <<Buf/binary, $\t>>);
 decode_string(<<$\\,  H, T/binary>>, Buf) -> decode_string(T, <<Buf/binary, H>>);
-decode_string(<<H, T/binary>>, Buf) -> decode_string(T, <<Buf/binary, H>>).
+decode_string(<<H, T/binary>>, Buf) -> decode_string(T, <<Buf/binary, H>>);
+decode_string(<<>>, _) -> error(unterminated_string).
 
-decode_list(<<H, T/binary>>, List) when ?is_space(H); H == $, -> decode_list(T, List);
-decode_list(<<$], T/binary>>, List) -> {T, lists:reverse(List)};
-decode_list(Bin, List) ->
+decode_array(<<H, T/binary>>, List) when ?is_space(H) -> decode_array(T, List);
+decode_array(<<$], T/binary>>, List) -> {T, lists:reverse(List)};
+decode_array(<<>>, _) -> error(unterminated_array);
+decode_array(Bin, List) ->
     {Rest, Value} = decode_value(Bin),
-    decode_list(Rest, [Value|List]).
+    decode_array(Rest, [Value|List]).
 
-decode_map(<<H, T/binary>>, Map) when ?is_space(H); H == $, -> decode_map(T, Map);
-decode_map(<<$}, T/binary>>, Map) -> {T, Map};
-decode_map(Bin, Map) ->
+decode_object(<<H, T/binary>>, Map) when ?is_space(H) -> decode_object(T, Map);
+decode_object(<<$}, T/binary>>, Map) -> {T, Map};
+decode_object(<<>>, _) -> error(unterminated_object);
+decode_object(Bin, Map) ->
     {Rest1, Key} = decode_value(Bin),
-    {Rest2, ok} = decode_colon(Rest1),
+    {Rest2, $:} = decode_colon(Rest1),
     {Rest3, Value} = decode_value(Rest2),
-    decode_map(Rest3, maps:put(Key, Value, Map)).
+    decode_object(Rest3, maps:put(Key, Value, Map)).
 
-decode_colon(<<$:, T/binary>>) -> {T, ok};
-decode_colon(<<H, T/binary>>) when ?is_space(H) -> decode_colon(T).
+decode_colon(<<$:, T/binary>>) -> {T, $:};
+decode_colon(<<H, T/binary>>) when ?is_space(H) -> decode_colon(T);
+decode_colon(_) -> error(missing_colon).
